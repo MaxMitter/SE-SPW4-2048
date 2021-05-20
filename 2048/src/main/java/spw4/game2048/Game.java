35e1f994b60612401bcec215833867382903d490
Max Mitter <max.mitter14@gmail.com>
2021-05-20 21:37:19 +0200

    Implements game

--- 2048/src/main/java/spw4/game2048/Game.java
+++ 2048/src/main/java/spw4/game2048/Game.java
@@ -1,36 +1,215 @@
 package spw4.game2048;
 
+import java.util.Random;
+
 public class Game {
+    private int[][] field;
+    private int score;
+    private int moves;
+    private Random random;
+    private boolean isOver;
+
     public Game() {
-        // to do ...
+        random = new Random();
     }
 
     public int getScore() {
-        // to do ...
-        return 0;
+        return score;
     }
 
     public boolean isOver() {
-        // to do ...
-        return false;
+        if (isWon())
+            return true;
+        else
+            return isOver;
     }
 
     public boolean isWon() {
-        // to do ...
+        for(int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                if (field [x][y] == 2048) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public String toString() {
-        // to do ...
-        return "";
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Moves: ").append(moves).append("\tScore: ").append(getScore()).append("\n");
+        for(int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                if (field[x][y] == 0)
+                    sb.append(".\t");
+                else
+                    sb.append(field[x][y]).append("\t");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public void initialize() {
-        // to do ...
+        field = new int[][]{{0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0}};
+        score = 0;
+        moves = 0;
+        isOver = false;
+
+        try{
+//            placeRandomTile();
+//            placeRandomTile();
+        } catch (Exception ignored) { } //ignored because calls will always succeed
+    }
+
+    private void placeRandomTile() throws NoFreeTileException{
+        if (isOver())
+            return;
+
+        if (!hasEmptyField()) throw new NoFreeTileException();
+
+        int x = random.nextInt(4);
+        int y = random.nextInt(4);
+        while (field[x][y] != 0) {
+            x = random.nextInt(4);
+            y = random.nextInt(4);
+        }
+
+        int poss = random.nextInt(10);
+        if (poss < 9)
+            field[x][y] = 2;
+        else
+            field[x][y] = 4;
+    }
+
+    private boolean hasEmptyField() {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                if (field[x][y] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public void move(Direction direction) {
-        // to do ...
+        boolean hasMoved = false;
+        switch (direction) {
+            case up: {
+                for (int currentCellCol = 0; currentCellCol < 4; currentCellCol++) {
+                    for (int currentCellRow = 0; currentCellRow < 4; currentCellRow++) {
+                        for (int checkRow = currentCellRow + 1; checkRow < 4; checkRow++) {
+                            if (field[currentCellRow][currentCellCol] == 0) {
+                                if (field[checkRow][currentCellCol] != 0) {
+                                    field[currentCellRow][currentCellCol] = field[checkRow][currentCellCol];
+                                    field[checkRow][currentCellCol] = 0;
+                                    hasMoved = true;
+                                }
+                            } else {
+                                if (field[currentCellRow][currentCellCol] == field[checkRow][currentCellCol]) {
+                                    field[currentCellRow][currentCellCol] = field[currentCellRow][currentCellCol] * 2;
+                                    score+= field[currentCellRow][currentCellCol];
+                                    field[checkRow][currentCellCol] = 0;
+                                    hasMoved = true;
+                                    break;
+                                } else if (field[checkRow][currentCellCol] != 0) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+            case down: {
+                for (int currentCellCol = 3; currentCellCol >= 0; currentCellCol--) {
+                    for (int currentCellRow = 3; currentCellRow >= 0; currentCellRow--) {
+                        for (int checkRow = currentCellRow - 1; checkRow >= 0; checkRow--) {
+                            if (field[currentCellRow][currentCellCol] == 0) {
+                                if (field[checkRow][currentCellCol] != 0) {
+                                    field[currentCellRow][currentCellCol] = field[checkRow][currentCellCol];
+                                    field[checkRow][currentCellCol] = 0;
+                                }
+                            } else {
+                                if (field[currentCellRow][currentCellCol] == field[checkRow][currentCellCol]) {
+                                    field[currentCellRow][currentCellCol] = field[currentCellRow][currentCellCol] * 2;
+                                    field[checkRow][currentCellCol] = 0;
+                                    score+= field[currentCellRow][currentCellCol];
+                                    break;
+                                } else if (field[checkRow][currentCellCol] != 0) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+            case left: {
+                for (int currentCellRow = 0; currentCellRow < 4; currentCellRow++) {
+                    for (int currentCellCol = 0; currentCellCol < 4; currentCellCol++) {
+                        for (int checkCol = currentCellCol + 1; checkCol < 4; checkCol++) {
+                            if (field[currentCellRow][currentCellCol] == 0) {
+                                if (field[currentCellRow][checkCol] != 0) {
+                                    field[currentCellRow][currentCellCol] = field[currentCellRow][checkCol];
+                                    field[currentCellRow][checkCol] = 0;
+                                }
+                            } else {
+                                if (field[currentCellRow][currentCellCol] == field[currentCellRow][checkCol]) {
+                                    field[currentCellRow][currentCellCol] = field[currentCellRow][currentCellCol] * 2;
+                                    score += field[currentCellRow][currentCellCol];
+                                    field[currentCellRow][checkCol] = 0;
+                                    break;
+                                } else if (field[currentCellRow][checkCol] != 0) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+            case right: {
+                for (int currentCellRow = 3; currentCellRow >= 0; currentCellRow--) {
+                    for (int currentCellCol = 3; currentCellCol >= 0; currentCellCol--) {
+                        for (int checkCol = currentCellCol - 1; checkCol >= 0; checkCol--) {
+                            if (field[currentCellRow][currentCellCol] == 0) {
+                                if (field[currentCellRow][checkCol] != 0) {
+                                    field[currentCellRow][currentCellCol] = field[currentCellRow][checkCol];
+                                    field[currentCellRow][checkCol] = 0;
+                                }
+                            } else {
+                                if (field[currentCellRow][currentCellCol] == field[currentCellRow][checkCol]) {
+                                    field[currentCellRow][currentCellCol] = field[currentCellRow][currentCellCol] * 2;
+                                    score += field[currentCellRow][currentCellCol];
+                                    field[currentCellRow][checkCol] = 0;
+                                    break;
+                                } else if (field[currentCellRow][checkCol] != 0) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+        if (hasMoved) {
+            try{
+                moves++;
+                placeRandomTile();
+            } catch (NoFreeTileException exception) {
+                isOver = true;
+            }
+        }
     }
+
+    private static class NoFreeTileException extends Exception { }
 }
---
+++ 2048/src/test/java/spw4/game2048/GameTest.java
@@ -0,0 +1,83 @@
+package spw4.game2048;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class GameTest {
+    private Game g;
+
+    @Mock
+    Game g_mock;
+
+    @BeforeEach
+    void beforeEach() {
+        g = new Game();
+    }
+
+    @Test
+    @DisplayName("getScore returns 0 on new Game")
+    public void getScore_OnNewGame_ReturnsZero() {
+        assertEquals(0, g.getScore());
+    }
+
+    @Test
+    @DisplayName("isOver returns true when game is over")
+    public void isOver_WhenGameBoardFull_ReturnsTrue() {
+        when(g_mock.isOver()).thenReturn(true);
+
+        assertTrue(g_mock.isOver());
+    }
+
+    @Test
+    @DisplayName("isOver returns false when game new")
+    public void isOver_WhenGameBoardNotFull_ReturnsFalse() {
+        assertFalse(g.isOver());
+    }
+
+    @Test
+    @DisplayName("isWon returns true when game is won")
+    public void isWon_WhenGameBoardHas2048_ReturnsTrue() {
+        when(g_mock.isWon()).thenReturn(true);
+
+        assertTrue(g_mock.isWon());
+    }
+
+    @Test
+    @DisplayName("isWon returns false when game is not won")
+    public void isWon_WhenGameBoardHasNo2048_ReturnsFalse() {
+        assertTrue(g.isWon());
+    }
+
+    @Test
+    @DisplayName("toString calls getScore Method exactly once")
+    public void toString_WhenGameIsRunning_CallsGetScoreOnce() {
+        g_mock.toString();
+
+        assertAll(
+                () -> verify(g_mock, times(1)).getScore()
+        );
+    }
+
+    @Test
+    @DisplayName("isOver calls isWon exactly once")
+    public void isOver_WhenGameIsRunning_CallsIsWonOnce() {
+        g_mock.isOver();
+
+        assertAll(
+                () -> verify(g_mock, times(1)).isOver()
+        );
+    }
+
+    @Test
+    @DisplayName("initialize places two random tiles")
+    public void initialize_WhenGameStarts_CallsPlaceRandomTileTwice() {
+        g.initialize();
+
+        int nonNull = 0;
+
+    }
+}
